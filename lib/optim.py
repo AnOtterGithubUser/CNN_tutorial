22c6daab558dff934e5310c2663ec2a2181959b1
AnOtterGithubUser <thomaspesneau@gmail.com>
2021-02-10 20:31:08 +0100

    Implement RMSProp

--- lib/optim.py
+++ lib/optim.py
@@ -40,11 +40,11 @@ class SGD(Optimizer):
 
 class Adam(Optimizer):
 
-    def __init__(self, seq, lr=0.01, betas=(0.9, 0.999), eps=10e-8):
+    def __init__(self, seq, lr=0.01, betas=(0.9, 0.999), epsilon=10e-8):
         super().__init__(seq)
         self._lr = lr
         self._betas = betas
-        self._eps = eps
+        self._epsilon = epsilon
         self._state = {}
 
     def get_state(self, key, grad_shape):
@@ -58,10 +58,34 @@ class Adam(Optimizer):
         beta1, beta2 = self._betas
         v, s, n_it = self.get_state(key, grad_parameter.shape)
         v = beta1 * v + (1 - beta1) * grad_parameter
-        s = beta2 * s + (1 - beta2) * grad_parameter * grad_parameter
+        s = beta2 * s + (1 - beta2) * grad_parameter ** 2
         n_it += 1
         self.set_state(key, (v, s, n_it))
         v_corrected = v / (1 - beta1 ** n_it)
         s_corrected = s / (1 - beta2 ** n_it)
-        new_parameter = parameter + self._lr * (v_corrected / np.sqrt(s_corrected) + self._eps)
+        new_parameter = parameter + self._lr * (v_corrected / np.sqrt(s_corrected) + self._epsilon)
+        return new_parameter
+
+
+class RMSProp(Optimizer):
+
+    def __init__(self, seq, lr, gamma, epsilon):
+        super().__init__(seq)
+        self._lr = lr
+        self._gamma = gamma
+        self._epsilon = epsilon
+        self._state = {}
+
+    def get_state(self, key, grad_shape):
+        return self._state[key] if key in self._state else np.zeros(grad_shape)
+
+    def set_state(self, key, values):
+        self._state[key] = values
+
+    def update(self, parameter, grad_parameter, layer_id, weight_type):
+        key = '{}_{}'.format(layer_id, weight_type)
+        s = self.get_state(key, grad_parameter.shape)
+        s = self._gamma * s + (1 - self._gamma) * grad_parameter ** 2
+        self.set_state(key, s)
+        new_parameter = parameter - grad_parameter * (self._lr / np.sqrt(s + self._epsilon))
         return new_parameter
